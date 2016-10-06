49ef8b2f37a18f051d6ae26e833310ec9b1bdfe0
Aaron Matthew Kaiser <aaronkaiser@me.com>
2016-10-06 13:41:39 -0700

    AntiKaiser 2016
    New design for AntiKaiser in 2016 (one-page design)

---
+++ antikaiser2016/front-page.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * @package AntiKaiser_2016
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="container">
+				<div class="columns">
+					<div>
+						<svg id="filmReel" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 434.06 434.04">
+							<title>Film Reel</title>
+							<path class="a" d="M410.16,461.77a4.59,4.59,0,1,0,1.67,6.27A4.6,4.6,0,0,0,410.16,461.77Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M419.9,454.2a4.59,4.59,0,1,0,4.59,4.59A4.59,4.59,0,0,0,419.9,454.2Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M405.57,475.66a4.59,4.59,0,1,0,6.27,1.68A4.59,4.59,0,0,0,405.57,475.66Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M417.6,476.62a4.59,4.59,0,1,0-1.68-6.27A4.59,4.59,0,0,0,417.6,476.62Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M367.28,467.52a21.86,21.86,0,0,0,21.87-37.86c-10.46-6-18.22.79-24.26,11.24S356.83,461.48,367.28,467.52Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M441.76,520.78a21.86,21.86,0,1,0-43.72,0c0,12.08,9.79,15.38,21.86,15.38S441.76,532.85,441.76,520.78Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M397.15,485.78a21.86,21.86,0,0,0-29.87-8c-10.46,6-8.43,16.17-2.39,26.62s13.79,17.28,24.25,11.24A21.86,21.86,0,0,0,397.15,485.78Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M442.65,459.51a21.86,21.86,0,0,0,29.86,8c10.46-6,8.42-16.17,2.39-26.63s-13.79-17.28-24.24-11.24A21.85,21.85,0,0,0,442.65,459.51Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M472.51,477.78a21.86,21.86,0,0,0-21.86,37.86c10.46,6,18.21-.78,24.24-11.24S483,483.82,472.51,477.78Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M501.48,717a219,219,0,0,0,26.86-1.82l-22.15-28.47Z" transform="translate(-282.97 -282.97)"/>
+							<rect class="a" x="173.28" y="301.79" width="86.53" height="17.03"/>
+							<path class="a" d="M467.44,714.6a218.28,218.28,0,0,0,26.38,2.34l-4.7-30.22Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="431.93" cy="465.74" r="4.58" transform="translate(-457.99 -4.54) rotate(-30.01)"/>
+							<path class="a" d="M434.23,475.67a4.59,4.59,0,1,0,1.68,6.27A4.58,4.58,0,0,0,434.23,475.67Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M419.9,446.37a21.85,21.85,0,0,0,21.86-21.86c0-12.07-9.78-15.38-21.86-15.38S398,412.44,398,424.52A21.86,21.86,0,0,0,419.9,446.37Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="136.93" cy="203.62" r="4.59"/>
+							<path class="a" d="M575.14,476.54a3.29,3.29,0,1,0-1.2,4.49A3.29,3.29,0,0,0,575.14,476.54Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="555.04" cy="478.18" r="3.29" transform="translate(-419.56 436.81) rotate(-60)"/>
+							<path class="a" d="M563.67,469.91a3.29,3.29,0,1,0,3.28,3.29A3.29,3.29,0,0,0,563.67,469.91Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="555.04" cy="488.14" r="3.29" transform="translate(-452.62 59.64) rotate(-29.98)"/>
+							<path class="a" d="M563.67,502A15.67,15.67,0,0,0,548,517.63c0,8.65,7,11,15.67,11s15.67-2.37,15.67-11A15.67,15.67,0,0,0,563.67,502Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="563.67" cy="483.13" r="3.29" transform="translate(-419.49 446.89) rotate(-60.01)"/>
+							<path class="a" d="M526,479.45a15.67,15.67,0,1,0,15.67-27.13c-7.49-4.33-13.06.56-17.38,8.06S518.47,475.13,526,479.45Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M547.36,492.54A15.68,15.68,0,0,0,526,486.81c-7.49,4.33-6,11.59-1.71,19.08s9.89,12.39,17.38,8.06A15.67,15.67,0,0,0,547.36,492.54Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M601.37,486.81a15.67,15.67,0,0,0-15.66,27.14c7.49,4.33,13.05-.56,17.38-8.06S608.87,491.14,601.37,486.81Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M585.71,452.32a15.67,15.67,0,0,0,15.66,27.13c7.49-4.32,6-11.59,1.71-19.08S593.2,448,585.71,452.32Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M500,283c-119.86,0-217,97.17-217,217,0,105.82,75.74,193.94,176,213.13l16.14-37.46h45.16l16.48,38.25C639.09,696.48,717,607.35,717,500,717,380.14,619.86,283,500,283ZM349.05,472.65A70.85,70.85,0,1,1,419.9,543.5,70.84,70.84,0,0,1,349.05,472.65Zm56.1,123.61-20.25,5.53V568.65l20.25,6Zm115.09,73.39H475.08V651.59h45.16Zm61.23-34.12-9,11H423.89l-14-6V560.25L466,540.17h73.27l42.16,16.06Zm29.89-33.74H587.43V568.65h23.93ZM563.67,533.9a50.77,50.77,0,1,1,50.77-50.77A50.77,50.77,0,0,1,563.67,533.9Zm87.28,80.77H640.82l-22.09-12.89V568.65l22.09-13.81h10.13Z" transform="translate(-282.97 -282.97)"/>
+							<path class="a" d="M563.67,464.3a15.66,15.66,0,0,0,15.66-15.67c0-8.65-7-11-15.66-11S548,440,548,448.63A15.67,15.67,0,0,0,563.67,464.3Z" transform="translate(-282.97 -282.97)"/>
+							<circle class="a" cx="572.29" cy="488.14" r="3.29" transform="translate(-419.63 456.55) rotate(-59.99)"/>
+							<path class="a" d="M563.67,496.41a3.29,3.29,0,1,0-3.29-3.29A3.29,3.29,0,0,0,563.67,496.41Z" transform="translate(-282.97 -282.97)"/>
+						</svg>
+					</div>
+					<div>
+						<?php $recent = new WP_Query("page_id=1310"); while($recent->have_posts()) : $recent->the_post();
+							the_content();
+						endwhile; ?>
+					</div>
+				</div>
+			</div>
+		</main><!-- #main -->
+		<section id="reel">
+			<div class="container">
+				<h3>
+					Watch our Sizzle Reel
+				</h3>
+				<div class="video-container">
+					<iframe src="https://www.youtube.com/embed/7vifP7Z_3JQ?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</section>
+		<section id="news">
+			<div class="container">
+				<h3>
+					Our Latest News
+				</h3>
+				<ul>
+				<?php $the_query = new WP_Query( 'posts_per_page=-1' ); ?>
+				<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+				<li>
+					<a href="<?php the_permalink() ?>"><?php the_date(); ?></a>
+					<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+				</li>
+				<?php
+					endwhile;
+					wp_reset_postdata();
+				?>
+				</ul>
+			</div>
+		</section>
+	</div><!-- #primary -->
+
+<?php
+get_footer();
